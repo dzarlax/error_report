67ff83290ed6a67825e3e7a90d77906ca8616705
313457 <Aleksey.Panfilov@westernunion.ru>
2018-08-03 13:46:18 +0300

    Setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Error report',
+    version='',
+    packages=['pandas','matplotlib'],
+    url='',
+    license='',
+    author='313457',
+    author_email='',
+    description=''
+)
